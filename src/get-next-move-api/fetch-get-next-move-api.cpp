fd71eb0cbd11f76da5acee39e8eb82e8543bb9c8
Mark <m4rc0d3r@gmail.com>
2025-05-24 23:52:09 +0300

    Implement getting the next move for the bot in the browser

--- src/get-next-move-api/fetch-get-next-move-api.cpp
+++ src/get-next-move-api/fetch-get-next-move-api.cpp
@@ -5,6 +5,32 @@
 
 using json = nlohmann::json;
 
+
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+
+EM_ASYNC_JS(char*, getBestMove, (const char* url, const char* fen, const char* depth), {
+    const ERROR = stringToNewUTF8("error");
+
+    const query = new URLSearchParams();
+    query.append("fen", UTF8ToString(fen));
+    query.append("depth", UTF8ToString(depth));
+    const response = await fetch([UTF8ToString(url), query.toString()].join("?"));
+    if (!response.ok) return ERROR;
+
+    const data = await response.json();
+    if (!data.success) return ERROR;
+
+    const bestmove = data.bestmove;
+    if (typeof bestmove !== "string") return ERROR;
+
+    console.log("Запит успішно виконано. Результат:", JSON.stringify(data));
+
+    return stringToNewUTF8(bestmove.substring(9, 13));
+});
+#endif // __EMSCRIPTEN__
+
 const string URL = "https://stockfish.online/api/s/v2.php";
 
 string FetchGetNextMoveApi::get(const string& fen, int levelOfDifficulty)
@@ -17,7 +43,11 @@ string FetchGetNextMoveApi::get(const string& fen, int levelOfDifficulty)
     printf("fen: \"%s\".\n", fen.c_str());
     printf("depth: \"%s\".\n", depth.c_str());
 
-    return ERROR;
+    auto data = getBestMove(URL.c_str(), fen.c_str(), depth.c_str());
+    string dataAsString = data;
+    free(data);
+
+    return dataAsString;
 }
 
 #endif // __EMSCRIPTEN__
